d6fa513d8f937784ee2d8fa61bae433a87e475d2
Alexander <sany199574@gmail.com>
2024-12-21 04:51:43 +0300

    Добавлен мапинг  - CurrencyMappingConfig

---
+++ src/MoneyMate_Simple/PS.MoneyMate_Simple/Mappings/CurrencyMappingConfig.cs
@@ -0,0 +1,12 @@
+﻿using Mapster;
+
+namespace PS.MoneyMate_Simple.Mappings
+{
+    public class CurrencyMappingConfig : IRegister
+    {
+        public void Register(TypeAdapterConfig config)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
