a7f66bcc1e0f7873215970d84d93d50da32c64a2
Alexander <sany199574@gmail.com>
2024-12-21 04:26:03 +0300

    Добавлена сущнось  - Currency

---
+++ src/MoneyMate_Simple/PS.MoneyMate_Simple/Entities/Currency.cs
@@ -0,0 +1,11 @@
+﻿namespace PS.MoneyMate_Simple.Entities
+{
+    public class Currency
+    {
+        public Guid Id { get; set; } = Guid.NewGuid(); // Уникальный идентификатор
+        public string Code { get; set; } = string.Empty; // Код валюты (например, USD)
+        public string Name { get; set; } = string.Empty; // Название валюты (например, "United States Dollar")
+        public string Symbol { get; set; } = string.Empty; // Символ валюты (например, "$")
+        public bool IsActive { get; set; } = true; // Флаг активности
+    }
+}
