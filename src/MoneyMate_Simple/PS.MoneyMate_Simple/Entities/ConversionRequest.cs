69faa28c89913051ec96eaef2d56807ffe1988db
Alexander <sany199574@gmail.com>
2024-12-21 04:26:43 +0300

    добавлена сущность  - ConversionRequest

---
+++ src/MoneyMate_Simple/PS.MoneyMate_Simple/Entities/ConversionRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace PS.MoneyMate_Simple.Entities
+{
+    public class ConversionRequest
+    {
+        public Guid Id { get; set; } = Guid.NewGuid(); // Уникальный идентификатор
+        public Guid FromCurrencyId { get; set; } // Валюта, из которой конвертируем
+        public Guid ToCurrencyId { get; set; } // Валюта, в которую конвертируем
+        public decimal Amount { get; set; } // Сумма, которую конвертируем
+        public decimal ConvertedAmount { get; set; } // Результат конвертации
+        public DateTime ConversionDate { get; set; } = DateTime.UtcNow; // Дата выполнения конверсии
+    }
+}
