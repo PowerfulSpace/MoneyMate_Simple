16ee6e5a1f1f6efd9a741ababd90683ab5573a62
Alexander <sany199574@gmail.com>
2024-12-21 04:27:09 +0300

    Добавлена сущность  - ExchangeRate

---
+++ src/MoneyMate_Simple/PS.MoneyMate_Simple/Entities/ExchangeRate.cs
@@ -0,0 +1,11 @@
+﻿namespace PS.MoneyMate_Simple.Entities
+{
+    public class ExchangeRate
+    {
+        public Guid Id { get; set; } = Guid.NewGuid(); // Уникальный идентификатор
+        public Guid FromCurrencyId { get; set; } // Валюта, из которой конвертируем
+        public Guid ToCurrencyId { get; set; } // Валюта, в которую конвертируем
+        public decimal Rate { get; set; } // Курс обмена
+        public DateTime LastUpdated { get; set; } = DateTime.UtcNow; // Последнее обновление курса
+    }
+}
