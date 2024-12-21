a34453e3cce926c4d02dc47f0005108097825cf8
Alexander <sany199574@gmail.com>
2024-12-21 04:51:08 +0300

    Добавлен мапинг  - ExchangeRateMappingConfig

---
+++ src/MoneyMate_Simple/PS.MoneyMate_Simple/Mappings/ConversionRequestMappingConfig.cs
@@ -0,0 +1,12 @@
+﻿using Mapster;
+
+namespace PS.MoneyMate_Simple.Mappings
+{
+    public class ConversionRequestMappingConfig : IRegister
+    {
+        public void Register(TypeAdapterConfig config)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/MoneyMate_Simple/PS.MoneyMate_Simple/Mappings/ExchangeRateMappingConfig.cs
@@ -0,0 +1,12 @@
+﻿using Mapster;
+
+namespace PS.MoneyMate_Simple.Mappings
+{
+    public class ExchangeRateMappingConfig : IRegister
+    {
+        public void Register(TypeAdapterConfig config)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
