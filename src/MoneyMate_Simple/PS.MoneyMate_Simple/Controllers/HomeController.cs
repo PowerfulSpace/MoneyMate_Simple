bbccdca3ddee4119393b2790f83eb45991e90b4a
Alexander <sany199574@gmail.com>
2024-12-21 04:16:29 +0300

    Добавлено приложение  - MoneyMate_Simple

---
+++ src/MoneyMate_Simple/PS.MoneyMate_Simple/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using PS.MoneyMate_Simple.Models;
+using System.Diagnostics;
+
+namespace PS.MoneyMate_Simple.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
